dd36da0ec9245b97a2e8ba07e99ceba55cd4417a
shinkawa-shinji <kumagai.s@wse-developer.com>
2021-12-23 22:12:43 +0900

    コンポーネント化、Todo編集

--- src/components/MeasuredItems.tsx
+++ src/components/MeasuredItems.tsx
@@ -13,7 +13,6 @@ import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import { v4 as uuidv4 } from "uuid";
 import { useUser } from "@/src/lib/auth";
 import {
-  addMeasuredItem,
   updateMeasuredItem,
   deleteMeasuredItem,
   updateMeasuredTime,
@@ -39,14 +38,9 @@ import {
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Select from "@mui/material/Select";
 
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import DateAdapterMoment from "@mui/lab/AdapterMoment";
-import DatePicker from "@mui/lab/DatePicker";
-import Slider from "@mui/material/Slider";
-
-import AddTodoIconComponent from "@/src/components/measure/AddTodoIcon";
+import AddTodoIcon from "@/src/components/measure/AddTodoIcon";
 import TodoItem from "@/src/components/measure/TodoItem";
 import NoteIcon from "@/src/components/measure/NoteIcon";
 import AddItem from "@/src/components/measure/AddItem";
@@ -92,20 +86,6 @@ const MeasuredItems = () => {
     dueDate: Date | number | null; // 上書き
     finishedDate?: Date | number | null; // 上書き
   };
-  const newTodoDefaultValues: ClientTodo = {
-    itemId: "",
-    subItemId: "",
-    _id: "",
-    description: null,
-    estimatedTime: 0,
-    unit: "MINUTES",
-    status: "NOT_STARTED",
-    dueDate: null,
-    finishedDate: null,
-  };
-  const [newTodo, setNewTodo] =
-    React.useState<ClientTodo>(newTodoDefaultValues);
-  const [editTodoDialog, setEditTodoDialog] = React.useState(false);
 
   const colorList = [
     "#3f51b5",
@@ -116,140 +96,6 @@ const MeasuredItems = () => {
     "#673ab7",
     "#757575",
   ];
-
-  const todoUnits: {
-    [key: string]: string;
-  } = {
-    MINUTES: "分",
-    HOURS: "時間",
-    PERSON_DAY: "人日",
-  };
-
-  const changeTodoTimeUnit = (
-    event: SelectChangeEvent
-    // child?: object
-  ): void => {
-    const newUnit = event.target.value;
-    if (!newUnit) return;
-    setNewTodo({
-      ...newTodo,
-      unit: newUnit as "MINUTES" | "HOURS" | "PERSON_DAY",
-      estimatedTime: 0,
-    });
-  };
-  const todoTimeConfig = (
-    unit: "MINUTES" | "HOURS" | "PERSON_DAY",
-    config: "min" | "max" | "step"
-  ) => {
-    if (unit === "MINUTES") {
-      if (config === "min") return 0;
-      if (config === "max") return 60;
-      if (config === "step") return 5;
-    }
-    if (unit === "HOURS") {
-      if (config === "min") return 0;
-      if (config === "max") return 10;
-      if (config === "step") return 0.5;
-    }
-    if (unit === "PERSON_DAY") {
-      if (config === "min") return 0;
-      if (config === "max") return 30;
-      if (config === "step") return 0.5;
-    }
-  };
-  const timeSliderValue = (value: number | null) => {
-    if (value === null) return 0;
-    return value;
-  };
-  const handleChangeNewTodo = (event: any) => {
-    const newDescription = event.target?.value;
-    setNewTodo({
-      ...newTodo,
-      description: newDescription,
-    });
-  };
-  const handleChangeNewTodoTimeSlider = (
-    event: Event,
-    value: number | Array<number>
-  ) => {
-    if (!event) return;
-    if (Array.isArray(value)) return;
-    setNewTodo({
-      ...newTodo,
-      estimatedTime: value,
-    });
-  };
-
-  const closeEditTodoDialog = () => {
-    setEditTodoDialog(false);
-    setNewTodo(newTodoDefaultValues);
-  };
-  const handleClickUpdateTodoButton = () => {
-    const item = items.find((item: Item) => item["_id"] === newTodo.itemId);
-    if (!item) return;
-    const newItem = { ...item };
-    const subItem = newTodo.subItemId
-      ? item.subItems.find(
-          (subItem: SubItem) => subItem["_id"] === newTodo.subItemId
-        )
-      : null;
-    const newSubItem = subItem ? { ...subItem } : subItem;
-    const newTodoObj = {
-      _id: newTodo._id,
-      description: newTodo.description,
-      estimatedTime: newTodo.estimatedTime,
-      unit: newTodo.unit,
-      status: newTodo.status,
-      dueDate:
-        newTodo.dueDate !== null ? new Date(newTodo.dueDate).getTime() : null,
-      finishedDate: null,
-    };
-
-    let newTodos = [],
-      newItems = [],
-      newSubItems = [];
-
-    if (!newTodo.subItemId) {
-      if (!item.todos) return;
-      newTodos = updateListOfObjects({
-        listOfObjects: [...item.todos],
-        newObject: newTodoObj,
-        filter: { key: "_id", value: newTodo._id },
-        processType: "REPLACE",
-      });
-
-      newItem.todos = newTodos;
-    }
-    if (newTodo.subItemId) {
-      if (!newSubItem || !subItem || !subItem.todos) return;
-      newTodos = updateListOfObjects({
-        listOfObjects: [...subItem.todos],
-        newObject: newTodoObj,
-        filter: { key: "_id", value: newTodo._id },
-        processType: "REPLACE",
-      });
-      newSubItem.todos = newTodos;
-      newSubItems = updateListOfObjects({
-        listOfObjects: [...item.subItems],
-        newObject: newSubItem,
-        filter: { key: "_id", value: newTodo.subItemId },
-        processType: "REPLACE",
-      });
-      newItem.subItems = newSubItems;
-    }
-
-    newItems = updateListOfObjects({
-      listOfObjects: [...items],
-      newObject: newItem,
-      filter: { key: "_id", value: newTodo.itemId as string },
-      processType: "REPLACE",
-    });
-
-    updateMeasuredItem(user.uid, newItems);
-    setNewTodo(newTodoDefaultValues);
-
-    closeEditTodoDialog();
-  };
 
   /**
    * Delete
@@ -577,92 +423,6 @@ const MeasuredItems = () => {
 
   return (
     <>
-      {/* Todo編集フォーム */}
-      <Dialog open={editTodoDialog} onClose={closeEditTodoDialog} fullWidth>
-        <DialogTitle>Todoの編集</DialogTitle>
-        <DialogContent>
-          <DialogContentText>Todoアイテムを編集しよう！！</DialogContentText>
-          <TextField
-            margin="dense"
-            name="editTodoDescription"
-            label="タイトル"
-            type="text"
-            fullWidth
-            variant="standard"
-            value={newTodo?.description}
-            onChange={handleChangeNewTodo}
-            required
-          />
-          <Box sx={{ py: 1 }}>
-            <LocalizationProvider
-              name="startTime"
-              dateAdapter={DateAdapterMoment as any}
-            >
-              <DatePicker
-                // minDate={new Date().getTime()} // TODO 設定する 2021/12/19
-                label="期日"
-                value={newTodo?.dueDate}
-                onChange={(value) => {
-                  if (value)
-                    setNewTodo({
-                      ...newTodo,
-                      dueDate: value,
-                    });
-                }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
-          </Box>
-          このタスクにおける見積時間
-          <Box sx={{ py: 1 }} display="flex" alignItems="center">
-            <FormControl variant="standard" sx={{ p: 1, width: 50 }}>
-              <TextField
-                type="text"
-                variant="standard"
-                value={newTodo?.estimatedTime}
-                disabled
-              />
-            </FormControl>
-            <FormControl variant="standard" sx={{ p: 1 }}>
-              <Select
-                name="unit"
-                value={newTodo?.unit}
-                onChange={changeTodoTimeUnit}
-              >
-                {(() => {
-                  const menuItems: any = [];
-                  Object.keys(todoUnits).forEach((key) => {
-                    menuItems.push(
-                      <MenuItem key={key} value={key}>
-                        {todoUnits[key]}
-                      </MenuItem>
-                    );
-                  });
-                  return menuItems;
-                })()}
-              </Select>
-            </FormControl>
-          </Box>
-          <Box sx={{ py: 1 }}>
-            時間
-            <Slider
-              aria-label="todoTime"
-              onChange={handleChangeNewTodoTimeSlider}
-              value={timeSliderValue(newTodo.estimatedTime)}
-              step={todoTimeConfig(newTodo.unit, "step")}
-              min={todoTimeConfig(newTodo.unit, "min")}
-              max={todoTimeConfig(newTodo.unit, "max")}
-              marks
-              valueLabelDisplay="auto"
-            />
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={closeEditTodoDialog}>戻る</Button>
-          <Button onClick={handleClickUpdateTodoButton}>更新</Button>
-        </DialogActions>
-      </Dialog>
-
       {/* 項目 編集フォーム */}
       <Dialog open={editDialog} onClose={closeEditDialog} fullWidth>
         <DialogTitle>項目の編集</DialogTitle>
@@ -846,7 +606,7 @@ const MeasuredItems = () => {
               {/* アイテム表示 */}
               <Grid item xs={12} sx={{ display: "flex" }}>
                 <NoteIcon item={item} />
-                <AddTodoIconComponent itemId={item._id} />
+                <AddTodoIcon itemId={item._id} />
                 <Button
                   variant={isActive ? "outlined" : "contained"}
                   onClick={
@@ -925,7 +685,7 @@ const MeasuredItems = () => {
                     >
                       {/* サブアイテム */}
                       <Grid item xs={10} sx={{ display: "flex" }}>
-                        <AddTodoIconComponent
+                        <AddTodoIcon
                           itemId={item._id}
                           subItemId={subItem._id}
                         />
